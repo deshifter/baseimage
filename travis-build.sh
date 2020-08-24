f4e7e89298c506fb620700bc8329a13e645fb238
Artem Fufaldin <artem.fufaldin@gmail.com>
2020-08-25 01:11:10 +0500

    Move scripts to config file

--- travis-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#### halt script on error
-set -ev
-
-#### Build the Docker Images
-if [ -n "${UBUNTU_VERSION}" ]; then
-    docker build -t deshifter/baseimage --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} .
-fi
